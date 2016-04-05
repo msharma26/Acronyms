dc0ce66ff0497296db6fcc9add4d56706bc32ba8
Manu Sharma <manusharma.aug26@gmail.com>
2016-04-05 01:31:20 -0400

    Initial Commit

---
+++ Acronyms/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Acronyms
+//
+//  Created by Manu Sharma on 4/5/16.
+//  Copyright © 2016 Manu Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Acronyms/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Acronyms
+//
+//  Created by Manu Sharma on 4/5/16.
+//  Copyright © 2016 Manu Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
