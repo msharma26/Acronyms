d65bbe098b61c9e3ccccf996fdfc03f77218b6c4
Manu Sharma <manusharma.aug26@gmail.com>
2016-04-09 15:28:19 -0400

    Adding a model class for sub node.

--- Acronyms/LfVarModel.h
+++ Acronyms/LfVarModel.h
@@ -1,19 +1,20 @@
 //
-//  FullFormNode.h
+//  lfsVarModel.h
 //  Acronyms
 //
-//  Created by Manu Sharma on 4/5/16.
+//  Created by Manu Sharma on 4/9/16.
 //  Copyright © 2016 Manu Sharma. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-@interface flsNode : NSObject
+@interface LfVarModel : NSObject
 
 - (id) initWithDict: (NSDictionary*) dict;
 
 @property (nonatomic, strong) NSString *fullForm;
-@property (nonatomic, strong) NSMutableArray *variations;
+@property (nonatomic, strong) NSString *since;
 @property (nonatomic, strong) NSString *freq;
+
 
 @end
